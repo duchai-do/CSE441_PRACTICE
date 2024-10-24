12e3c89efc77d3b68cd5d146ec223061ef83cecf
duchai-do <haitong192@gmail.com>
2024-10-24 10:40:05 +0700

    bt10
    bt10

---
+++ BT10/app/src/main/java/com/example/bt10/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bt10;
+
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_MMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_MMS}, 1);
+            return;
+        }
+    }
+}
